27fc1b3b072550b2fae29e68dea0e4459c3cf835
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:54 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -290,10 +290,35 @@ declare module 'react-native-ico-shopping' {
       'discount-5' |
       'money-bag-5';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -301,4 +326,3 @@ declare module 'react-native-ico-shopping' {
     export { iconNames };
     export default Icon;
 }
-
